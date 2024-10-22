b04670a101284f8269d69de75ab09e15b113a2bf
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-10-22 10:20:48 +0200

    skapade scanner och lade till instruktioner i metoderna TvSeries och addEpisodes.

--- src/TVSeries.java
+++ src/TVSeries.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TVSeries {
+    // Keyboard
+    Scanner keyboard = new Scanner(System.in);
+
     // Attributes
     private String name;
     private ArrayList<Integer> episodes = new ArrayList<>();
@@ -8,13 +12,15 @@ public class TVSeries {
 
     // Methods
     public void TvSeries(){
-        //jj
+        System.out.print("Please insert the name of the Series here: ");
+        name = keyboard.nextLine();
     }
     public void TvSeries(String name){
         //jj
     }
     public void addEpisodes(int numOfEpisodes, int season){
-        //jj
+        episodes.add(season-1,numOfEpisodes);
+        System.out.println("The number of episodes, "+numOfEpisodes+", of  season "+season+" was added.");
     }
     public void changeRating(){
         //jj
