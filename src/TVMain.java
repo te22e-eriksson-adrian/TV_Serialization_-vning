fab8f175061d64ada9bbd50e013653e223c0d2e4
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2024-11-03 10:04:41 +0100

    fick programmet att fungera i alla delar bortsett från serialiseringen som ej är påbörjad.

--- src/TVMain.java
+++ src/TVMain.java
@@ -1,12 +1,72 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class TVMain {
     // Attributes
     private ArrayList<TVSeries> shows = new ArrayList<>();
 
     // Methods
-    public void TvMain(){
-        //jj
+    public TVMain(){
+        TVSeries user1 = new TVSeries();
+        String name;
+        
+        Scanner keyboard = new Scanner(System.in);
+        int choice = 0;
+        
+        while (choice != 5) {
+            System.out.println();
+            System.out.println("Menu: (Please, select a number to initiate the corresponding action!)");
+            System.out.println(" 1. Save a Tv Series.\n 2. Add amount of episodes and seasons for a Tv Series.\n 3. Change the rating and amount of episodes on saved Tv Series.\n 4. Print info. \n 5. Shut down Television.");
+            System.out.print("Choice: ");
+            try{
+                choice = keyboard.nextInt();
+                keyboard.nextLine();
+            }catch(Exception e){
+                choice = 4;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Name of Tv Series: ");
+                    name = keyboard.nextLine();
+                    System.out.println();
+                    System.out.print("Amount of seasons: ");
+                    int season = keyboard.nextInt();
+                    keyboard.nextLine();
+                    System.out.println("Amount of episodes: ");
+                    int numOfEpisodes = keyboard.nextInt();
+                    keyboard.nextLine();
+
+                    shows.add(new TVSeries(name, numOfEpisodes, season));
+                    break;
+                case 2:
+                    System.out.println("Steps to add information about a Tv Series:");
+                    System.out.println();
+                    System.out.print("Amount of seasons: ");
+                    season = keyboard.nextInt();
+                    keyboard.nextLine();
+                    System.out.println("Amount of episodes: ");
+                    numOfEpisodes = keyboard.nextInt();
+                    keyboard.nextLine();
+                    
+                    user1.addEpisodes(numOfEpisodes, season);
+                    break;
+                case 3:
+                    System.out.print("New rating: ");
+                    int newRating = keyboard.nextInt();
+                    keyboard.nextLine();
+
+                    user1.changeRating(newRating);
+                    break;
+                case 4:
+                    user1.printInfo();
+                    break;
+                default:
+                System.out.println("You made a faulty input, please enter the number corresponding to the alternative in the menu.");
+                    break;
+            }
+        }
+        keyboard.close();
     }
     public void Write2File(){
         //jj
--- src/TVSeries.java
+++ src/TVSeries.java
@@ -11,21 +11,28 @@ public class TVSeries {
     private int rating;
 
     // Methods
-    public void TvSeries(){
+    public TVSeries(){
         System.out.print("Please insert the name of the Series here: ");
         name = keyboard.nextLine();
     }
-    public void TvSeries(String name){
-        //jj
+    public TVSeries(String name, int episodes, int season){
+        this.name = name;
+        addEpisodes(episodes, season);
     }
+
     public void addEpisodes(int numOfEpisodes, int season){
         episodes.add(season-1,numOfEpisodes);
         System.out.println("The number of episodes, "+numOfEpisodes+", of  season "+season+" was added.");
     }
-    public void changeRating(){
-        //jj
+    public void changeRating(int newRating){
+        rating = newRating;
+        System.out.println("Current rating: "+newRating);
     }
     public void printInfo(){
-        //jj
+        System.out.println("Information about the Tv Series:");
+        System.out.println("- "+name+"\n- "+rating);
+        for (Integer integer : episodes) {
+            System.out.println(" - "+integer);
+        }
     }
 }
