79cfdc0b236982f311e76bc295e2716369fc584f
te22e-eriksson-adrian <adrian.eriksson@elev.ga.ntig.se>
2025-03-19 11:20:06 +0100

    finish

--- src/TVMain.java
+++ src/TVMain.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class TVMain{
     // Attributes
     private ArrayList<TVSeries> shows = new ArrayList<>();
-    TVSeries user1 = new TVSeries();
+    //TVSeries user1 = new TVSeries();
 
     // Methods
     public TVMain(){
@@ -13,6 +13,8 @@ public class TVMain{
         
         Scanner keyboard = new Scanner(System.in);
         int choice = 0;
+
+        createShowData();
         
         while (choice != 5) {
             System.out.println();
@@ -23,7 +25,7 @@ public class TVMain{
                 choice = keyboard.nextInt();
                 keyboard.nextLine();
             }catch(Exception e){
-                choice = 4;
+                choice = 5;
             }
 
             switch (choice) {
@@ -41,6 +43,17 @@ public class TVMain{
                     shows.add(new TVSeries(name, numOfEpisodes, season));
                     break;
                 case 2:
+                // user first neeeds tochoos which show to work with
+                    System.out.println("Available shows:");
+                    for(TVSeries series : shows){
+                        System.out.println(" - " + series.getName());
+                    }
+                    System.out.println("Wich show would you like to edit (enter a number based on the order of the displayed alternatives:)");
+                    System.out.print("Choice: ");
+                    int showChoice = keyboard.nextInt();
+                    keyboard.nextLine();
+                    TVSeries chosenShow = shows.get(showChoice-1);
+
                     System.out.println("Steps to add information about a Tv Series:");
                     System.out.println();
                     System.out.print("Amount of seasons: ");
@@ -49,18 +62,22 @@ public class TVMain{
                     System.out.println("Amount of episodes: ");
                     numOfEpisodes = keyboard.nextInt();
                     keyboard.nextLine();
+
+                    //shows.set(showChoice-1, chosenShow.addEpisodes(numOfEpisodes, season));
                     
-                    user1.addEpisodes(numOfEpisodes, season);
+                    //user1.addEpisodes(numOfEpisodes, season);
                     break;
                 case 3:
+                // user first neeeds to choos which show to work with
                     System.out.print("New rating: ");
                     int newRating = keyboard.nextInt();
                     keyboard.nextLine();
 
-                    user1.changeRating(newRating);
+                   // user1.changeRating(newRating);
                     break;
                 case 4:
-                    user1.printInfo();
+                // user first neeeds to choos which show to work with
+                    //user1.printInfo();
                     break;
                 default:
                 System.out.println("You made a faulty input, please enter the number corresponding to the alternative in the menu.");
@@ -69,13 +86,15 @@ public class TVMain{
         }
         keyboard.close();
     }
-    public void createShowData(){   //?
-        //jj
+    public void createShowData(){
+        shows.add( new TVSeries("Air Crash Investigation", 120, 6));
+        shows.add( new TVSeries("Blacklist", 150, 11));
+        shows.add( new TVSeries("Lost", 80, 6));
     }
     public void Write2File() throws IOException{   //Serializing
         FileOutputStream fileOut = new FileOutputStream("TVSeriesInfo.ser");
         ObjectOutputStream out = new ObjectOutputStream(fileOut);
-        out.writeObject(user1);
+        out.writeObject(shows);
         out.close();
         fileOut.close();
     }
@@ -83,7 +102,7 @@ public class TVMain{
         FileInputStream fileIn = new FileInputStream("C:\\Users\\adrian.eriksson\\Documents\\Java projects 2\\lekv_prog2_uppg2_klasser\\TVSeriesInfo.ser");
         ObjectInputStream in = new ObjectInputStream(fileIn);
         try {
-            user1 = (TVSeries) in.readObject();
+            shows = (ArrayList<TVSeries>) in.readObject();
         } catch (ClassNotFoundException e) {
             System.out.println("Reading of previously saved file failed!");
         }
--- src/TVSeries.java
+++ src/TVSeries.java
@@ -10,21 +10,24 @@ public class TVSeries implements Serializable{
     private String name;
     private ArrayList<Integer> episodes = new ArrayList<>();
     private int rating;
+    private int seasons;
 
     // Methods
     public TVSeries(){
         System.out.print("Please insert the name of the Series here: ");
         name = keyboard.nextLine();
     }
-    public TVSeries(String name, int episodes, int season){
+    public TVSeries(String name, int episodes, int seasons){
         this.name = name;
-        addEpisodes(episodes, season);
+        this.seasons = seasons;
+        addEpisodes(episodes, seasons);
     }
 
     public void addEpisodes(int numOfEpisodes, int season){
         episodes.add(season-1,numOfEpisodes);
         System.out.println("The number of episodes, "+numOfEpisodes+", of  season "+season+" was added.");
     }
+
     public void changeRating(int newRating){
         rating = newRating;
         System.out.println("Current rating: "+newRating);
@@ -35,5 +38,8 @@ public class TVSeries implements Serializable{
         for (Integer integer : episodes) {
             System.out.println(" - "+integer);
         }
+    }
+    public String getName(){
+        return name;
     }
 }
